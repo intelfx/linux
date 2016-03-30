4e3b2f7ad5eacc159e3a66a60b15594d4d8f8aef
Edward Shishkin <edward.shishkin@gmail.com>
2016-03-30 11:20:23 +0200

    reiser4: port for Linux-4.5
    . Adjust for the new ->get_link() of vfs's inode_operations: we don't
       support RCU mode;
    . check returned value of __copy_from_user() to make gcc happy
       (the path is never invoked) in create_item_node40().
    
    Signed-off-by: Edward Shishkin <edward.shishkin@gmail.com>

--- fs/reiser4/plugin/object.h
+++ fs/reiser4/plugin/object.h
@@ -25,7 +25,8 @@ int reiser4_mknod_common(struct inode *parent, struct dentry *dentry,
 int reiser4_rename2_common(struct inode *old_dir, struct dentry *old_name,
 			   struct inode *new_dir, struct dentry *new_name,
 			   unsigned flags);
-const char *reiser4_follow_link_common(struct dentry *, void **cookie);
+const char *reiser4_get_link_common(struct dentry *, struct inode *inode,
+				    struct delayed_call *done);
 int reiser4_permission_common(struct inode *, int mask);
 int reiser4_setattr_common(struct dentry *, struct iattr *);
 int reiser4_getattr_common(struct vfsmount *mnt, struct dentry *,
