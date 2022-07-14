9901111f149219a9c1593e3264f6008985e27a15
Daniel Hill <daniel@gluo.nz>
2022-07-14 20:33:09 +1200

    bcachefs: added lock held time stats
    We now record the length of time btree locks are held and expose this in debugfs.
    
    Enabled via CONFIG_BCACHEFS_LOCK_TIME_STATS.
    
    Signed-off-by: Daniel Hill <daniel@gluo.nz>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/bcachefs.h
+++ fs/bcachefs/bcachefs.h
@@ -319,6 +319,8 @@ BCH_DEBUG_PARAMS_DEBUG()
 #undef BCH_DEBUG_PARAM
 #endif
 
+#define BCH_LOCK_TIME_NR 128
+
 #define BCH_TIME_STATS()			\
 	x(btree_node_mem_alloc)			\
 	x(btree_node_split)			\
@@ -527,6 +529,11 @@ enum {
 
 struct btree_debug {
 	unsigned		id;
+};
+
+struct lock_held_stats {
+	struct bch2_time_stats	times[BCH_LOCK_TIME_NR];
+	const char		*names[BCH_LOCK_TIME_NR];
 };
 
 struct bch_fs_pcpu {
@@ -922,6 +929,8 @@ mempool_t		bio_bounce_pages;
 	bool			promote_whole_extents;
 
 	struct bch2_time_stats	times[BCH_TIME_STAT_NR];
+
+	struct lock_held_stats	lock_held_stats;
 };
 
 static inline void bch2_set_ra_pages(struct bch_fs *c, unsigned ra_pages)
