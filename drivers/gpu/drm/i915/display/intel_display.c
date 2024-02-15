e4fda19c50d4c7afec0c97cfcbd46a59860d8d5b
Ivan Shapovalov <intelfx@intelfx.name>
2024-02-15 04:38:42 +0100

    Revert "drm/i915: Compute clocks earlier"
    This reverts commit 0ff0e219d9b8db047d3e800553f238136ed53ed7.

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -2470,10 +2470,6 @@ static int intel_crtc_compute_config(struct intel_atomic_state *state,
 	if (ret)
 		return ret;
 
-	ret = intel_dpll_crtc_compute_clock(state, crtc);
-	if (ret)
-		return ret;
-
 	ret = intel_crtc_compute_pipe_src(crtc_state);
 	if (ret)
 		return ret;
@@ -6518,6 +6514,10 @@ int intel_atomic_check(struct drm_device *dev,
 					    new_crtc_state, i) {
 		if (intel_crtc_needs_modeset(new_crtc_state)) {
 			any_ms = true;
+
+			ret = intel_dpll_crtc_compute_clock(state, crtc);
+			if (ret)
+				goto fail;
 
 			intel_dpll_release(state, crtc);
 			continue;
