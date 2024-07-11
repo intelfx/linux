56c6ac5d7c10f1e91aa21cde885ff4194908d051
Ivan Shapovalov <intelfx@intelfx.name>
2024-07-11 23:04:14 +0200

    zswap: add global zswap.writeback_disable parameter
    Add a `zswap.writeback_disable` module parameter to control zswap
    writeback behavior globally. To make the behavior late-binding,
    internally introduce a third value for `memory.zswap.writeback` cgroup
    attribute that means "follow the global configuration", and use it as
    the default value for the root cgroup.
    
    Reads from `memory.zswap.writeback` cgroup attribute will be coerced to
    0 or 1 to maintain backwards compatilibity. However, it is possible to
    write -1 to that attribute to make the cgroup follow the default again.
    
    Signed-off-by: Ivan Shapovalov <intelfx@intelfx.name>

--- include/linux/zswap.h
+++ include/linux/zswap.h
@@ -36,6 +36,7 @@ void zswap_memcg_offline_cleanup(struct mem_cgroup *memcg);
 void zswap_lruvec_state_init(struct lruvec *lruvec);
 void zswap_folio_swapin(struct folio *folio);
 bool is_zswap_enabled(void);
+bool is_zswap_writeback_enabled(void);
 #else
 
 struct zswap_lruvec_state {};
@@ -63,6 +64,11 @@ static inline void zswap_folio_swapin(struct folio *folio) {}
 static inline bool is_zswap_enabled(void)
 {
 	return false;
+}
+
+static inline bool is_zswap_writeback_enabled(void)
+{
+	return true;
 }
 
 #endif
