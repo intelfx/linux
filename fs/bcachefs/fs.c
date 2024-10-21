7fa450415ac4212f384a98b9c429cd12bcba81f1
Kent Overstreet <kent.overstreet@linux.dev>
2024-10-20 20:27:44 -0400

    bcachefs: Don't delete reflink pointers to missing indirect extents
    To avoid tragic loss in the event of transient errors (i.e., a btree
    node topology error that was later corrected by btree node scan), we
    can't delete reflink pointers to correct errors.
    
    This adds a new error bit to bch_reflink_p, indicating that it is known
    to point to a missing indirect extent, and the error has already been
    reported.
    
    Indirect extent lookups now use bch2_lookup_indirect_extent(), which on
    error reports it as a fsck_err() and sets the error bit, and clears it
    if necessary on succesful lookup.
    
    This also gets rid of the bch2_inconsistent_error() call in
    __bch2_read_indirect_extent, and in the reflink_p trigger: part of the
    online self healing project.
    
    An on disk format change isn't necessary here: setting the error bit
    will be interpreted by older versions as pointing to a different index,
    which will also be missing - which is fine.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/fs.c
+++ fs/bcachefs/fs.c
@@ -1258,7 +1258,6 @@ static int bch2_fiemap(struct inode *vinode, struct fiemap_extent_info *info,
 	struct btree_iter iter;
 	struct bkey_s_c k;
 	struct bkey_buf cur, prev;
-	unsigned offset_into_extent, sectors;
 	bool have_extent = false;
 	int ret = 0;
 
@@ -1305,9 +1304,8 @@ static int bch2_fiemap(struct inode *vinode, struct fiemap_extent_info *info,
 			continue;
 		}
 
-		offset_into_extent	= iter.pos.offset -
-			bkey_start_offset(k.k);
-		sectors			= k.k->size - offset_into_extent;
+		s64 offset_into_extent	= iter.pos.offset - bkey_start_offset(k.k);
+		unsigned sectors	= k.k->size - offset_into_extent;
 
 		bch2_bkey_buf_reassemble(&cur, c, k);
 
@@ -1319,7 +1317,7 @@ static int bch2_fiemap(struct inode *vinode, struct fiemap_extent_info *info,
 		k = bkey_i_to_s_c(cur.k);
 		bch2_bkey_buf_realloc(&prev, c, k.k->u64s);
 
-		sectors = min(sectors, k.k->size - offset_into_extent);
+		sectors = min_t(unsigned, sectors, k.k->size - offset_into_extent);
 
 		bch2_cut_front(POS(k.k->p.inode,
 				   bkey_start_offset(k.k) +
