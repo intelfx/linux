0340a8fb4f19932869c84c14a946b0912f90683a
Ivan Shapovalov <intelfx@intelfx.name>
2024-11-06 19:28:09 +0400

    ACPI: utils: use NULL-entry-terminated tables in _OSC pretty-printer

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -703,7 +703,7 @@ struct acpi_osc_bit_struct {
 };
 void acpi_decode_osc_bits(struct acpi_device *device, acpi_handle handle,
 			  char *msg, u32 word,
-			  struct acpi_osc_bit_struct *table, int size);
+			  struct acpi_osc_bit_struct *table);
 
 /* Table must be terminted by a NULL entry */
 struct acpi_platform_list {
