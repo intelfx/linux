4946e8f62966a711872f46fd75303bd0d8845ffe
Ivan Shapovalov <intelfx@intelfx.name>
2024-12-04 16:37:16 +0400

    fs: introduce FOP_REMAP_CROSS_SB (no-op)
    Signed-off-by: Ivan Shapovalov <intelfx@intelfx.name>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2127,6 +2127,8 @@ struct file_operations {
 #define FOP_UNSIGNED_OFFSET	((__force fop_flags_t)(1 << 5))
 /* Supports asynchronous lock callbacks */
 #define FOP_ASYNC_LOCK		((__force fop_flags_t)(1 << 6))
+/* Supports cross-sb remap */
+#define FOP_REMAP_CROSS_SB      ((__force fop_flags_t)(1 << 7))
 
 /* Wrap a directory iterator that needs exclusive inode access */
 int wrap_directory_iterator(struct file *, struct dir_context *,
