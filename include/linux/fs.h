ab518a1dc47deb7d099275dd60f00953f9712cd7
Ivan Shapovalov <intelfx@intelfx.name>
2024-12-04 16:30:36 +0400

    fs: consolidate same-f_op and same-sb checks for FI{CLONE,DEDUPE}*
    Export the same-sb check as generic_file_remap_checks() and make use of
    it in remap_range.c as well. This is more complicated because unlike
    generic_copy_file_checks(), the remap checks are split into multiple
    functions.
    
    This affects the FICLONE/FICLONERANGE/FIDEDUPERANGE entry points.
    
    Part of openzfs/zfs#15345.
    
    Signed-off-by: Ivan Shapovalov <intelfx@intelfx.name>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3042,6 +3042,7 @@ int generic_write_checks_count(struct kiocb *iocb, loff_t *count);
 extern int generic_write_check_limits(struct file *file, loff_t pos,
 		loff_t *count);
 extern int generic_file_rw_checks(struct file *file_in, struct file *file_out);
+extern int generic_file_remap_checks(struct file *file_in, struct file *file_out);
 ssize_t filemap_read(struct kiocb *iocb, struct iov_iter *to,
 		ssize_t already_read);
 extern ssize_t generic_file_read_iter(struct kiocb *, struct iov_iter *);
