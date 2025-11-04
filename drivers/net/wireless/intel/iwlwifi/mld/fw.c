ef9042c4ac2058e0ef12b1a39f92fa1e20b249b6
Ivan Shapovalov <intelfx@intelfx.name>
2025-11-04 03:04:10 +0100

    iwlwifi: support lar_disable for MLD devices

--- drivers/net/wireless/intel/iwlwifi/mld/fw.c
+++ drivers/net/wireless/intel/iwlwifi/mld/fw.c
@@ -470,7 +470,8 @@ static int iwl_mld_config_fw(struct iwl_mld *mld)
 	if (ret)
 		return ret;
 
-	iwl_mld_configure_lari(mld);
+	if (!iwlwifi_mod_params.lar_disable)
+		iwl_mld_configure_lari(mld);
 
 	ret = iwl_mld_config_temp_report_ths(mld);
 	if (ret)
